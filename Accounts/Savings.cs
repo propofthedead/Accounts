e662261a1bef12a88fd1024b305abdd87bdefad1
Foster Stule <camodude5@yahoo.com>
2018-06-05 08:49:07 -0400

    created subclass of account

---
+++ Accounts/Savings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+	class Savings :Account
+	{
+		protected decimal IntRate { get; set; }
+
+		public decimal CalcInterest(decimal years) {
+			decimal interest = balance*(1 + (IntRate * years));
+
+			return interest;
+		}
+
+		public void addInterest(decimal years) {
+			balance += CalcInterest(years);
+		}
+	}
+}
