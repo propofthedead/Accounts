ff141c4aaba4329402c176ddf8117a3dbc7cfd35
Foster Stule <camodude5@yahoo.com>
2018-06-05 08:57:48 -0400

    added a transactiong method

--- Accounts/Checking.cs
+++ Accounts/Checking.cs
@@ -8,6 +8,12 @@ namespace Accounts
 {
 	class Checking : Account
 	{
+		static int TransactionId = 0;
 
+		public void Tranaction(decimal amount) {
+			balance += amount;
+			++TransactionId;
+			Console.WriteLine($"The amount {amount} was either added or withdrawn from {base.id} to make the {base.balance} dollars");
+		}
 	}
 }
